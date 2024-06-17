e94d6c8529f58412d846970d15e8f054ae736fd1
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2024-06-17 13:48:11 -0600

    added login route and components v1

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import SignInForm from "../../components/Auth/SignInForm";
+
+const LoginPage = () => {
+	return (
+		<SignInForm
+			logoSrc="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+			brandName="DBWM"
+		/>
+	);
+};
+
+export default LoginPage;
