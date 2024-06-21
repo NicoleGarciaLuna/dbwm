c304584d86d80c10933d8113e2402c95ce808884
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2024-06-21 11:25:53 -0600

    Moved menu items to a separate data file.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,51 +1,11 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { DashboardIcon, KanbanIcon, InboxIcon, UsersIcon, ProductsIcon, SignInIcon, SignUpIcon } from "./icons/DashboardIcons";
+import { menuItems } from "../data/menuItems";
 
 type LayoutProps = {
 	children: ReactNode;
 };
-
-const menuItems = [
-	{
-		href: "#",
-		label: "Dashboard",
-		icon: <DashboardIcon />,
-	},
-	{
-		href: "#",
-		label: "Kanban",
-		icon: <KanbanIcon />,
-	},
-	{
-		href: "#",
-		label: "Inbox",
-		icon: <InboxIcon />,
-		badge: "3",
-		badgeColor: "text-blue-800 bg-blue-100 dark:bg-blue-900 dark:text-blue-300",
-	},
-	{
-		href: "#",
-		label: "Users",
-		icon: <UsersIcon />,
-	},
-	{
-		href: "#",
-		label: "Products",
-		icon: <ProductsIcon />,
-	},
-	{
-		href: "#",
-		label: "Sign In",
-		icon: <SignInIcon />,
-	},
-	{
-		href: "#",
-		label: "Sign Up",
-		icon: <SignUpIcon />,
-	},
-];
 
 const Layout = ({ children }: LayoutProps) => {
 	return (
