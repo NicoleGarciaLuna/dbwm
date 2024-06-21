5873f56a5c5d0e07149c477346fc17dc61a73144
caguevara11 <carlos.guevara@ucrso.info>
2024-06-21 17:26:39 -0600

    Updated header to show at left

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="flex items-center mx-auto max-w-screen-xl">
+        <div className="flex items-center w-full">
           <a href="#" className="flex items-center">
             <Image
               src="https://flowbite.com/docs/images/logo.svg"
