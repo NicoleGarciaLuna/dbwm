8f5177fa855a4ee55a664f37df73091aa9369f11
caguevara11 <carlos.guevara@ucrso.info>
2024-07-02 22:30:11 -0600

    Styling changed in table from flowbite to ant-d

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Layout from "@/components/Layout/Layout";
+import MicroentrepreneursList from "@/components/MicroentrepreneursList/MicroentrepreneursList";
+import { defaultData } from "@/data/defaultData";
 
 export default function Home() {
   return (
     <Layout>
-      <div></div>
+      <div>
+        <MicroentrepreneursList data={defaultData} />
+      </div>
     </Layout>
   );
 }
