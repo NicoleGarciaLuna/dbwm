0f50c0c716400f131ad5c2c92cb86a58ff0e4ac1
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2024-07-09 00:19:47 -0600

    data extracted from supabase

---
+++ utils/api/fetchTable.ts
@@ -0,0 +1,35 @@
+import { supabase } from "./supabaseClient";
+import { QueryData } from "@supabase/supabase-js";
+
+const personasConDatosQuery = supabase.from("persona").select(`
+  id_persona,
+  nombre,
+  primer_apellido,
+  segundo_apellido,
+  diagnostico (
+    emprendimiento (
+      nombre_emprendimiento,
+      tiempo_operacion,
+      sector_economico
+    ),
+    idea_negocio (
+      descripcion_breve
+    )
+  )
+`);
+
+export type PersonasConDatos = QueryData<typeof personasConDatosQuery>;
+
+export const fetchPersonasConDatos =
+	async (): Promise<PersonasConDatos | null> => {
+		try {
+			const { data, error } = await personasConDatosQuery;
+			if (error) {
+				throw error;
+			}
+			return data;
+		} catch (error) {
+			console.error("Error fetching data:", error);
+			return null;
+		}
+	};
