8792b15829ab085a5cedc2022544fa18fecd17d7
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2024-07-11 20:24:04 -0600

    Customized charts colors

--- components/charts/BarChartComponent.tsx
+++ components/charts/BarChartComponent.tsx
@@ -1,44 +1,50 @@
 import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
+	BarChart,
+	Bar,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+	Cell,
 } from "recharts";
 
 type BarChartData = {
-  name: string;
-  value: number;
+	name: string;
+	value: number;
 };
 
 type CustomBarChartProps = {
-  data: BarChartData[];
+	data: BarChartData[];
 };
 
 const CustomBarChart = ({ data }: CustomBarChartProps) => {
-  return (
-    <ResponsiveContainer width="100%" height={400}>
-      <BarChart
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="value" fill="#8884d8" />
-      </BarChart>
-    </ResponsiveContainer>
-  );
+	const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#8dd1e1"];
+
+	return (
+		<ResponsiveContainer width="100%" height={400}>
+			<BarChart
+				data={data}
+				margin={{
+					top: 5,
+					right: 30,
+					left: 20,
+					bottom: 5,
+				}}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="name" />
+				<YAxis />
+				<Tooltip />
+				<Bar dataKey="value">
+					{data.map((entry, index) => (
+						<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+					))}
+				</Bar>
+			</BarChart>
+		</ResponsiveContainer>
+	);
 };
 
 export default CustomBarChart;
