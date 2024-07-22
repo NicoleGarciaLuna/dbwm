16063889a9ce65e9c14045699f51694fc8f5e2c3
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2024-07-22 12:54:12 -0600

    Login Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import MicroentrepreneursList from "@/features/microentrepreneurs/components/List";
+import { redirect } from "next/navigation";
+import { createClient } from "@/shared/utils/supabase/server";
 
-export default function Home() {
-  return <MicroentrepreneursList />;
+export default async function Home() {
+	const supabase = createClient();
+
+	const { data, error } = await supabase.auth.getUser();
+	if (error || !data?.user) {
+		console.error(error);
+		redirect("/login");
+	}
+	return <MicroentrepreneursList />;
 }
