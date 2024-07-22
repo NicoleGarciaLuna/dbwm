4ce53b3fb6a88273c018995fb9ab747da6b4e241
caguevara11 <carlos.guevara@ucrso.info>
2024-07-22 14:55:41 -0600

    Added login middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 import MicroentrepreneursList from "@/features/microentrepreneurs/components/List";
-import { redirect } from "next/navigation";
-import { createClient } from "@/shared/utils/supabase/server";
 
 export default async function Home() {
-	const supabase = createClient();
-
-	const { data, error } = await supabase.auth.getUser();
-	if (error || !data?.user) {
-		console.error(error);
-		redirect("/login");
-	}
-	return <MicroentrepreneursList />;
+  return <MicroentrepreneursList />;
 }
