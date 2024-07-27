cc385470c46f0ef15e472675edc522de7a890e3a
caguevara11 <carlos.guevara@ucrso.info>
2024-07-27 12:03:57 -0600

    Create microentrepeneur and create diagnosis form

--- app/(layout)/questionnaire/page.tsx
+++ app/(layout)/questionnaire/page.tsx
@@ -1,81 +1,12 @@
 "use client";
-import QuestionTabs from "@/features/questionnaire/components/QuestionTabs";
-
-const data = {
-	Personal: [
-		{ question: "¿Cuál es tu nombre completo?", type: "fill", options: [] },
-		{ question: "¿Cuál es tu edad?", type: "fill", options: [] },
-		{ question: "¿Cuál es tu fecha de nacimiento?", type: "date", options: [] },
-	],
-	Género: [
-		{
-			question: "¿Con qué género te identificas?",
-			type: "single",
-			options: ["Masculino", "Femenino", "Otro"],
-		},
-	],
-	Emprendimiento: [
-		{
-			question: "¿Tienes experiencia previa en emprendimiento?",
-			type: "single",
-			options: ["Sí", "No"],
-		},
-		{
-			question: "¿Cuál es tu área de emprendimiento?",
-			type: "multiple",
-			options: ["Tecnología", "Salud", "Educación", "Alimentos", "Otro"],
-		},
-	],
-	"Idea de Negocio": [
-		{
-			question: "Describe brevemente tu idea de negocio.",
-			type: "fill",
-			options: [],
-		},
-	],
-	Innovación: [
-		{
-			question: "¿Tu idea de negocio es innovadora?",
-			type: "single",
-			options: ["Sí", "No"],
-		},
-	],
-	Mercado: [
-		{
-			question: "¿A qué mercado objetivo te diriges?",
-			type: "fill",
-			options: [],
-		},
-	],
-	"Contabilidad y Finanzas": [
-		{
-			question: "¿Tienes conocimientos en contabilidad y finanzas?",
-			type: "single",
-			options: ["Sí", "No"],
-		},
-	],
-	Formalización: [
-		{
-			question: "¿Tu negocio está formalizado?",
-			type: "single",
-			options: ["Sí", "No"],
-		},
-	],
-	Financiamiento: [
-		{
-			question: "¿Necesitas financiamiento para tu negocio?",
-			type: "single",
-			options: ["Sí", "No"],
-		},
-	],
-};
+import DiagnosisSelector from "@/features/questionnaire/components/DiagnosisSelector";
 
 const Questionnaire = () => {
 	return (
-		<div>
-			<QuestionTabs data={data} />
-		</div>
-	);
+    <div style={{ padding: "20px" }}>
+      <DiagnosisSelector />
+    </div>
+  );
 };
 
 export default Questionnaire;
--- features/questionnaire/components/QuestionForm.tsx
+++ features/questionnaire/components/QuestionForm.tsx
@@ -1,38 +1,90 @@
-import { Input, Select, Checkbox, DatePicker, Form } from "antd";
-
-const { Option } = Select;
-
-type Question = {
-	question: string;
-	type: "fill" | "single" | "multiple" | "date";
-	options: string[];
-};
+import { Col, Input, Select, Checkbox, DatePicker, Form } from "antd";
 
 type QuestionFormProps = {
-	questionData: Question;
+  questionData: Question;
+  diagnosis: number;
 };
 
-const QuestionForm = ({ questionData }: QuestionFormProps) => {
-	const { question, type, options } = questionData;
+type Question = {
+  id: number;
+  question: string;
+  type: "fill" | "single" | "multiple" | "date" | "number";
+  options?: Option[];
+  locked: boolean;
+  required: boolean;
+};
 
-	return (
-		<Form.Item label={question} style={{ marginBottom: "24px" }}>
-			{type === "fill" && <Input style={{ width: "100%" }} />}
-			{type === "single" && (
-				<Select style={{ width: "100%" }}>
-					{options.map((option, index) => (
-						<Option key={index} value={option}>
-							{option}
-						</Option>
-					))}
-				</Select>
-			)}
-			{type === "multiple" && (
-				<Checkbox.Group options={options} style={{ width: "100%" }} />
-			)}
-			{type === "date" && <DatePicker style={{ width: "100%" }} />}
-		</Form.Item>
-	);
+type Option = string | { text: string; value: any };
+
+const QuestionForm = ({ questionData, diagnosis }: QuestionFormProps) => {
+  const { question, type, options, locked, required } = questionData;
+  const isLocked = locked && diagnosis !== 1;
+
+  const renderQuestion = () => {
+    switch (type) {
+      case "fill":
+        return (
+          <Input
+            placeholder={`Ingrese su ${question.toLowerCase()}`}
+            disabled={isLocked}
+            type="text"
+          />
+        );
+      case "single":
+        return (
+          <Select placeholder="Seleccione una opción" disabled={isLocked}>
+            {options?.map((option) => (
+              <Select.Option
+                key={typeof option === "string" ? option : option.value}
+                value={typeof option === "string" ? option : option.value}
+              >
+                {typeof option === "string" ? option : option.text}
+              </Select.Option>
+            ))}
+          </Select>
+        );
+      case "multiple":
+        return (
+          <Checkbox.Group
+            options={options?.map((option) =>
+              typeof option === "string"
+                ? option
+                : { label: option.text, value: option.value }
+            )}
+            disabled={isLocked}
+          />
+        );
+      case "date":
+        return <DatePicker disabled={isLocked} placeholder="AAAA-MM-DD" />;
+      case "number":
+        return (
+          <Input
+            type="number"
+            placeholder={`Ingrese su ${question.toLowerCase()}`}
+            disabled={isLocked}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Col xs={24} sm={12} md={12} lg={6}>
+      <Form.Item
+        label={question}
+        name={question}
+        rules={[
+          {
+            required: required,
+            message: `Por favor, complete la pregunta.`,
+          },
+        ]}
+      >
+        {renderQuestion()}
+      </Form.Item>
+    </Col>
+  );
 };
 
 export default QuestionForm;
